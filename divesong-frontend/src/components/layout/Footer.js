99d64e9e716527ee885e4532e8fe65b5df8f402e
Moosa Mohamed <moosa009692@gmail.com>
2018-12-13 23:58:30 +0530

    Auto Change song Alpha

--- divesong-frontend/src/components/layout/Footer.js
+++ divesong-frontend/src/components/layout/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { config } from '../../config';
 import axios from 'axios';
+import { Helmet } from 'react-helmet';
 
 class Footer extends Component {
   state = {
@@ -15,30 +16,78 @@ class Footer extends Component {
     this.setState({
       song: res.data
     });
+
+    // const s = document.createElement('script');
+    // s.type = 'text/javascript';
+    // s.async = true;
+    // s.innerHTML =
+    //   'var aud = document.getElementById("myAudio");aud.onended = function() {alert("The audio has ended");};';
+    // document.body.appendChild(s);
   }
   render() {
     // const { branding } = this.props;
 
-    const { name, artists } = this.state.song;
+    const { name, artists, lplayed } = this.state.song;
+    console.log(lplayed);
 
     return (
-      <footer className="footer veryblack">
-        &nbsp;&nbsp;
-        <span className="text-muted">
-          {name} <br />
-          &nbsp;{artists}{' '}
-        </span>
-        <label htmlFor="switch" />
-        <div className="container">
-          <iframe
-            src={`http://${config.server.hostname}:${
-              config.server.port
-            }/playNextSong`}
-          >
-            {' '}
-          </iframe>
-        </div>
-      </footer>
+      <React.Fragment>
+        <footer className="footer veryblack">
+          &nbsp;&nbsp;
+          <span className="text-muted">
+            {name} <br />
+            &nbsp;{artists}{' '}
+          </span>
+          <label htmlFor="switch" />
+          <div className="container">
+            {/* {setTimeout(
+            () => (
+              console.log('Next Song In'),
+              (
+                <iframe
+                  src={`http://${config.server.hostname}:${
+                    config.server.port
+                  }/playNextSong`}
+                >
+                  {' '}
+                </iframe>
+              )
+            ),
+            duration * 1000
+          )} */}
+
+            {/* <iframe
+              src={`http://${config.server.hostname}:${
+                config.server.port
+              }/playNextSong`}
+            >
+              {' '}
+            </iframe> */}
+
+            <audio
+              id="myAudio"
+              controls
+              autoPlay
+            // onEnded={{
+            //   function() {
+            //     alert('Audio has ended');
+            //   }
+            // }}
+            >
+              <source id="mySource" src={`http://${config.server.hostname}:${config.server.port}/getNextSong`} type="audio/mp3" />
+            </audio>
+          </div>
+          {/* <script /> */}
+        </footer>
+
+        <Helmet>
+          <script>
+            {
+              `var aud = document.getElementById("myAudio"); aud.onended = function() {aud.innerHTML="<source id=\'mySource\'src={'http://'${config.server.hostname}:${config.server.port}/getNextSong'}type=\'audio/mp3\'/>"; console.log('Song Ended')};`
+            }
+          </script>
+        </Helmet>
+      </React.Fragment>
     );
   }
 }
